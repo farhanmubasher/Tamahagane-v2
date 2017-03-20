f28a1f33ec6ffdb000d2b395be5db3cb3c7fd89d
Farhan Mubasher <farhanmubasher308@gmail.com>
2017-03-20 17:44:29 +0500

    Add files via upload

---
+++ R/sort.R
@@ -0,0 +1,51 @@
+
+library(lubridate)
+
+tamahagane.apply.sort<- function(filePath, columName, sortType)
+{
+  Dataset <- read.csv(filePath)
+  columName <- fromJSON(columName)
+  sortType <- fromJSON(sortType)
+
+  check  <- tryCatch({
+    !all(is.na(as.Date(Dataset[, columName],format="%d/%m/%Y")))
+  }, error=function(e){NULL})
+
+  if(!is.null(check) && check!= FALSE)
+  {
+    Dataset[columName] <- dmy(Dataset[, columName])
+    count.misssing.values  <- colSums(is.na(Dataset[columName]))
+    if(count.misssing.values >= 1)
+    {
+      return ("Date Format is not Correct")
+    }
+    else
+    {
+
+      if(sortType=="ASC" || sortType=="")
+      {
+        return (Dataset[order(Dataset[, columName]) ,])
+      }
+      else(sortType=="DESC")
+      {
+        return (Dataset[rev(order(Dataset[, columName]))  ,])
+      }
+
+    }
+
+  }
+
+  else if (is.null(check) || check== FALSE){
+
+    if(sortType=="ASC" || sortType=="")
+    {
+      return (Dataset[order(Dataset[, columName]) ,])
+    }
+    else(sortType=="DESC")
+    {
+      return (Dataset[rev(order(Dataset[, columName]))  ,])
+    }
+  }
+
+
+}
