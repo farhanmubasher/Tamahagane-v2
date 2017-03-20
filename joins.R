ab4b0dec724619c1cc094c128fc20d23eef2a8b8
Farhan Mubasher <farhanmubasher308@gmail.com>
2017-03-20 17:27:13 +0500

    Add files via upload

---
+++ joins.R
@@ -0,0 +1,16 @@
+library(plyr)
+tamahagane.apply.join.updated.csvVersion<- function(filePath.1,filePath.2,joinType)
+{
+  x <- read.csv(filePath.1)
+  y <- read.csv(filePath.2)
+  joinType <- fromJSON(joinType)
+  result <- join(x, y, type= joinType)
+  result[is.na(result)] <- "?"
+  if(nrow(result) == 0){
+      
+    return("No Records Found !!")
+  }
+  else{
+    return(result)
+  }
+}
