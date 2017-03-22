18374945dc4f034436597c2f3cd48f2b0b010cc4
Farhan Mubasher <farhanmubasher308@gmail.com>
2017-03-22 16:17:05 +0500

    Write output into File of CSV

--- R/joins.R
+++ R/joins.R
@@ -1,16 +1,17 @@
+library(jsonlite)
 library(plyr)
-tamahagane.apply.join<- function(filePath.1,filePath.2,joinType)
+tamahagane.apply.join.updated.csvVersion<- function(inputFilePath.1, inputFilePath.2, joinType, outputFilePath)
 {
-  x <- read.csv(filePath.1)
-  y <- read.csv(filePath.2)
+  x <- read.csv(inputFilePath.1)
+  y <- read.csv(inputFilePath.2)
   joinType <- fromJSON(joinType)
   result <- join(x, y, type= joinType)
   result[is.na(result)] <- "?"
   if(nrow(result) == 0){
-
-    return("No Records Found !!")
+      
+    return( write.table( "No Records Found !!", file = outputFilePath, sep= ",", quote = FALSE, row.names = FALSE, col.names = FALSE))
   }
   else{
-    return(result)
+    return(write.csv(result, file = outputFilePath, quote = FALSE, row.names = FALSE) )
   }
 }
